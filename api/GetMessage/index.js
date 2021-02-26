fa8fd939ca24fe98b35517af6cfce64e0633af1d
David Gristwood <davidgristwood@hotmail.com>
2021-02-26 16:25:36 +0000

    <body> bug fix

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { text: "Helo from the API "}
+        body: { text: "Hello, hello, hello from the function"}
     };
 }
